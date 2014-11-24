a9555a197050a1f674ccf82fe15f51b1d299ac01
flint <im@fh.io>
2014-11-24 13:34:53 -0800

    Update index.php
    Added notice about https://github.com/J7mbo/twitter-api-php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once('TwitterAPIExchange.php');
+require_once('TwitterAPIExchange.php'); /** https://github.com/J7mbo/twitter-api-php **/
 
 /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
 $settings = array(
